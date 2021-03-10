dc051c155c5776a9bc3a0c7f3ad21e9913d655ab
VictorBoYang <tun27434@temple.edu>
2021-03-09 19:40:22 -0500

    added sortable table. Now user can sort the data by clicking the title of data

--- Bo_YB-Gunpla/web/js/components/UserFromJson.js
+++ Bo_YB-Gunpla/web/js/components/UserFromJson.js
@@ -1,27 +1,39 @@
 function UserFromJson() {
 
-    // ` this is a "back tick". Use it to define multi-line strings in JavaScript.
-    var content = `  
-        <style>
+// ` this is a "back tick". Use it to define multi-line strings in JavaScript.
+var content = `  
+    <style>
            
-            h1 {
+    h1 {
                 text-align: center;
                 padding-top: 70px;
             }
     
-            .clickSort td img { /* applies to any <img> tag in a <td> tag in any element classed "clickSort" */
-                width: 70px;
-                border-radius: 6px;
-                box-shadow: 3px 3px 3px #444444;
-            }
+    .clickSort {  /* The reusable code "claims" this one className 
+        The HTML coder can use these styles (to get default styling)
+        or change if they want to. */
+        text-align:center;
+        }
     
-    
-            .clickSort table {
-                margin:auto;     /* auto makes left and right margins the same, means centered */
-                margin-top: 1em; /* em means size of capital M */
-                width: 40%;
-            }
-     </style>
+    .clickSort table {
+    margin:auto;     /* auto makes left and right margins the same, means centered */
+    }
+
+    .clickSort td {  /* applies to any <td> tag in any element classed "clickSort" */
+        padding: 0.25rem;
+        background-color: #DDEEEE;
+        font-size:1rem;
+    }
+
+    .clickSort th {  /* applies to any <th> tag in any element classed "clickSort" */
+        padding:0.25rem;
+        padding-top:0.5rem;
+        background-color: #AABBCC;
+        font-weight: bold;
+        font-size:1.1rem;
+        white-space: nowrap; /* do not want multi word column headings (or sort icon) to wrap */
+    }
+    </style>
       <p>
         <h1>
             ---Find User---
@@ -31,49 +43,46 @@ function UserFromJson() {
       
       </p>
     `;
-    
-    "use strict"; // turn off the "auto variable declaration" feature of the browser.
+        "use strict"; // turn off the "auto variable declaration" feature of the browser.
 
-    // invoke ajax function to read cars.json and if the call was successful, 
-    // run function processJSON, otherwise, put an error message in the DOM element 
-    // that has id "listHere".
-    ajax("json/users.json", processData, document.getElementById("listHere"));
-
-    function processData(list) {
+        // invoke ajax function to read cars.json and if the call was successful, 
+        // run function processJSON, otherwise, put an error message in the DOM element 
+        // that has id "listHere".
+        ajax("json/users.json", processData, document.getElementById("listHere"));
+        function processData(list) {
 
         // print out JS object/array that was converted from JSON data by ajax function
         console.log(list);
+                // build new list as we want the fields to appear in the HTML table
+                // we can decide the order we want the fields to appear (first property defined is shown first)
+                // we can combine, decide to exclude etc...
+                var userList = [];
+                // modify properties (image and price) of the array of objects so it will look 
+                // better on the page.
+                for (var i = 0; i < list.length; i++) {
 
-        // build new list as we want the fields to appear in the HTML table
-        // we can decide the order we want the fields to appear (first property defined is shown first)
-        // we can combine, decide to exclude etc...
-        var userList = [];
+        userList[i] = {};
+                // Don't show the id (no meaningful data)
+                userList[i].image = "<img  src='" + list[i].image + "' style='width:10rem'>";
+                userList[i].userEmail = list[i].userEmail; // show this first
+                // Don't show the password
+                userList[i].birthday = list[i].birthday;
+                userList[i].role = list[i].userRoleId + " " + list[i].userRoleType;
+        }
 
-        // modify properties (image and price) of the array of objects so it will look 
-        // better on the page.
-        for (var i = 0; i < list.length; i++) {
-
-            userList[i] = {};
-            // Don't show the id (no meaningful data)
-            userList[i].image = "<img  src='" + list[i].image + "'>";
-            userList[i].userEmail = list[i].userEmail; // show this first
-            // Don't show the password
-            userList[i].birthday = list[i].birthday;
-            userList[i].role = list[i].userRoleId + " " + list[i].userRoleType;
+        function inject(ele, where) {
+        document.getElementById(where).innerHTML = ""; // blank out content area before appending
+                document.getElementById(where).appendChild(ele);
         }
 
         console.log("USER LIST");
-        console.log(userList);
-        
-        document.getElementById("listHere").appendChild(MakeTable(userList));
+                console.log(userList);
+                inject(MakeSortableTable(userList, "make", "icons/sortUpDown16.png"), "listHere");
+//        document.getElementById("listHere").appendChild(MakeTable(userList));
 
-    }
-    
-    var ele = document.createElement("div");
-    ele.innerHTML = content;
-    
-    return ele;
+        }
 
-
-
-}
+var ele = document.createElement("div");
+        ele.innerHTML = content;
+        return ele;
+        }
--- Bo_YB-Gunpla/web/js/reusable/MakeClickSortTable.js
+++ Bo_YB-Gunpla/web/js/reusable/MakeClickSortTable.js
@@ -1,14 +1,83 @@
-function MakeTable(list) {
+function MakeSortableTable(list, sortOrderPropName, sortIcon) {
+
+    function jsSort(list, byProperty) {
+
+        // To use the built-in sort method (that is available to any JS array),
+        // you pass it a function that compares two of the elements of the array
+        // and returns 1, 0 or -1 depending on how the two elements compare with each other.
+
+        list.sort(// takes in one parameter -- a function that compares two elements in the list...
+
+                function (q, z) {  // in line anonymous fn to compare list elements: 
+                    // return positive (if first bigger), 0 if equal, negative otherwise.
+
+                    // Remember q and z are arbitrary elements of list and our job is to say whether q is bigger or z... 
+                    // We are supposed to decide based on the value of "byProperty" (which is a string 
+                    // hoding the name of the property by which we want to sort). 
+                    // 
+                    // To get the actual values to compare, use JS associative array notation: z[byProperty}, z[byProperty].
+                    // 
+                    // Next, we add function "convert" into the mix. It checks the value (which may be 
+                    // a number or date stored in a String). It just converts the value to its actual data type, 
+                    // like numer or date, so it can compare accurately for the sort. 
+                    var qVal = convert(q[byProperty]);
+                    var zVal = convert(z[byProperty]);
+
+                    var c = 0;
+                    if (qVal > zVal) {
+                        c = 1;
+                    } else if (qVal < zVal) {
+                        c = -1;
+                    }
+                    console.log("comparing " + qVal + " to " + zVal + " is " + c);
+                    return c;
+                } // end of the anonymous comparision function
+        );
+
+        // check the string to see what type it is, then return that string converted to the right type 
+        // so as to get the sort order correct. 
+        function convert(s) {
+
+            if (!s || s.length === 0) {
+                //console.log("s is null or empty string");
+                return -1;
+            }
+
+            // a string that holds a date returns true for isNaN(strDate) (it's not a number)  
+            // And it returns false for isNaN(Date.parse(strDate))
+            var parsedDate = Date.parse(s);
+            if (isNaN(s) && !isNaN(parsedDate)) {
+                //console.log(s + " is a Date ");
+                return parsedDate;
+            } else {
+                var tmp = s;
+                console.log("tmp is " + tmp);
+                tmp = tmp.replace("$", ""); // remove dollar signs
+                tmp = tmp.replace(",", ""); // remove commas
+                if (isNaN(tmp)) { // if not a number, return what was passed in 
+                    //console.log(s + " is a string - convert to uppercase for sorting purposes");
+                    return s.toUpperCase();
+                } else {
+                    //console.log(tmp + " is a number");
+                    return Number(tmp);
+                }
+            }
+        } // convert 
+
+    } // jsSort
+
 
     // Add data as th or td (based on eleType) to row of HTML table.
-    function addToRow(eleType, row, data, align) {
+    function addToRow(eleType, row, data, alignment) {
         var ele = document.createElement(eleType);
         ele.innerHTML = data;
-        ele.style.textAlign = align;
+        ele.style.textAlign = alignment;
         row.appendChild(ele);
         return ele;  // future code may need a reference to this dom object
     }
 
+    // Return alignment ("left", "right", "center") based on data type in 
+    // val (e.g., if val contains "123" (which can be converted to numeric), return "right". 
     function alignment(val) {
 
         // check if date
@@ -18,7 +87,7 @@ function MakeTable(list) {
         }
 
         // check if image
-        if (val.includes(".png") || val.includes(".jpg")) {
+        if (val.toUpperCase().includes("<IMG")) {
             console.log('is center');
             return "center";
         }
@@ -27,68 +96,86 @@ function MakeTable(list) {
         var possibleNum = val.replace("$", "");
         possibleNum = possibleNum.replace(",", "");
         if (isNaN(possibleNum)) {
-            console.log("not a num - left");
             return "left";
         }
-
         return "right"; // it's a number
 
     } // alignment
 
-    function prettyColumnHeading(propName) {
+    function addTableHead(table, list) {
 
-        if (propName.length === 0) {
-            return "";
-        }
+        // Create a thead element, place it in table, then 
+        // fill up the thead with td elements that are column headers 
+        // (populated by the field names from the first object in list). 
+        var tableHead = document.createElement("thead");
+        table.appendChild(tableHead);
 
-        // capitalize first letter
-        var newHdg = propName.charAt(0).toUpperCase();
-        // iterate through all characters, inserting space before any capital letters.
-        for (var i = 1; i < propName.length; i++) {
-            if (propName.charAt(i) < "a") {
-                newHdg += " ";
-            }
-            newHdg += propName.charAt(i);
-        }
-        return newHdg;
-    } // prettyColumnHeading
+        var tableHeadRow = document.createElement("tr");
+        tableHead.appendChild(tableHeadRow);
 
-
-    // Main Program.
-
-    // Create a new HTML table (DOM object) and append 
-    // that into the page. 
-    var newTable = document.createElement("table");
-
-    // Create a header for table and put a row in the header
-    var tableHead = document.createElement("thead");
-    newTable.appendChild(tableHead);
-    var tableHeadRow = document.createElement("tr");
-    tableHead.appendChild(tableHeadRow);
-
-    // create one column header per property with column header content
-    // matching the property name
-    var obj = list[0];
-    for (var prop in obj) {
-        addToRow("th", tableHeadRow, prettyColumnHeading(prop), alignment(obj[prop]));
-    }
-
-    // Add one row (to HTML table) per element in the array.
-    // Each array element has a list of properties that will become 
-    // td elements (Table Data, a cell) in the HTML table. 
-    var tableBody = document.createElement("tbody");
-    newTable.appendChild(tableBody);
-    for (var i in list) {
-        var tableRow = document.createElement("tr");
-        tableBody.appendChild(tableRow);
-
-        // create one table data <td> content matching the property name
-        var obj = list[i];
+        // create one column header per property - column header will show the property name. 
+        var obj = list[0];
         for (var prop in obj) {
-            addToRow("td", tableRow, obj[prop], alignment(obj[prop]));
+            console.log("setting the sort onclick for column " + prop);
+            var iconProp = "<img src='" + sortIcon + "'/> " + prop;
+            var colHead = addToRow("th", tableHeadRow, iconProp, alignment(obj[prop]));
+
+            // place the property name right into the DOM element that is the "th"
+            // Because later when the "th" is clicked "prop" will be the last property
+            // (cause this for loop would have already completed). 
+            colHead.sortPropName = prop;
+            colHead.onclick = function () {
+                // "this" means the DOM lement clicked upon. Take the sortPropName (that
+                // we stored in the "th" when the "th" was originally made) and use that
+                // for sort order. 
+                console.log("ready to sort by " + this.sortPropName);
+                addTableBody(table, list, this.sortPropName);
+            };
         }
     }
 
+
+    // sort 'list' by 'sortOrderPropName', remove the tbody from 'table' (if there is one), 
+    // then build a new tbody (from the sorted list) and insert that tbody into the table.  
+    function addTableBody(table, list, sortOrderPropName) {
+
+        // remove old tbody element if there is one (else you'll get sorted rows added to end of what's there).
+        var oldBody = table.getElementsByTagName("tbody");
+        if (oldBody[0]) {
+            console.log("ready to remove oldBody");
+            table.removeChild(oldBody[0]);
+        }
+
+        jsSort(list, sortOrderPropName);
+
+        // Add one row (to HTML table) per element in the array.
+        // Each array element has a list of properties that will become 
+        // td elements (Table Data, a cell) in the HTML table. 
+        var tableBody = document.createElement("tbody");
+        table.appendChild(tableBody);
+
+        for (var i in list) {
+            var tableRow = document.createElement("tr");
+            tableBody.appendChild(tableRow);
+
+            // create one table data <td> content matching the property name
+            var obj = list[i];
+            for (var prop in obj) {
+                addToRow("td", tableRow, obj[prop], alignment(obj[prop]));
+            }
+        }
+
+    } // addTableBody
+
+
+    // Entry Point of MakeSortableTable
+    console.log("function MakeSortableTable called with initial sort: " + sortOrderPropName);
+
+    var newTable = document.createElement("table");
+    addTableHead(newTable, list);
+    addTableBody(newTable, list, sortOrderPropName);
+
+    // this can be injected into the content area by other code
     return newTable;
 
 }
